33f37222f5dd0e64bdccbe8d20de3b8652f03ca0
AnaghPaul <125043274+AnaghPaul@users.noreply.github.com>
2024-12-19 18:48:17 +0530

    Add cd command with some functionalities

--- shellby.js
+++ shellby.js
@@ -1,44 +1,67 @@
 const shellName = "shellby";
-let currDir = "~";
+const rootDir = '/Users/random';
+// const homeDir = '~';
+const currDir = ["~"];
 
 const getPrompt = function () {
-  return shellName + ' ' + currDir + ' %';
+  return shellName + ' ' + currDir.at(-1) + ' %';
 }
 
 const getNextCommand = function () {
   const command = prompt(getPrompt());
 
-  return command.split(' ');
+  return command.trim().split(' ');
 }
 
 const echo = function (args) {
   console.log(args.join(' '));
 }
 
-const cd = function (path) {
-  currDir = path;
+const cd = function (args) {
+  const path = args.join(' ').trim().split(' ');
+
+  for (const dir of path) {
+    if (dir === '..') {
+      if (currDir.length !== 1) {
+        currDir.pop();
+      }
+      continue;
+    }
+
+    currDir.push(dir);
+  }
 }
 
 const externCommands = function (command, args) {
-  console.log(command, ':command does not exist');
+  console.log(command + ': command does not exist');
+}
+
+const pwd = function () {
+  console.log(rootDir + '/' + currDir.join('/'));
+}
+
+const runCommand = function(currentCommand, args) {
+  switch (currentCommand) {
+    case '':
+      return;
+    case 'echo' : 
+      return echo(args);
+    case 'cd' : 
+      return cd(args.join('').split('/'));
+    case 'pwd':
+      return pwd();
+    default:
+      return externCommands(currentCommand, args);
+  }
 }
 
 function startShell() {
-  console.clear();
+  // console.clear();
 
   while (true) {
     const [currentCommand, ...args] = getNextCommand();
-  
-    switch (currentCommand) {
-      case 'echo' : 
-        echo(args);
-        break;
-      case 'cd' : 
-        cd(args);
-        break;
-      default:
-        externCommands(currentCommand, args);
-    }
+
+    runCommand(currentCommand, args);
   
     // console.log('Command entered', currentCommand, '| arguments', args);
   }
